7cc9f727458820a32c092983cdb09a82bc976fe6
letDouglas <damir.borsetti@refactormi.com>
2025-01-30 15:54:09 +0100

    fix bug eureka (#2)

--- src/main/java/com/ttf/VideoUpload/VideoUploadApplication.java
+++ src/main/java/com/ttf/VideoUpload/VideoUploadApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VideoUploadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VideoUploadApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VideoUploadApplication.class, args);
+    }
 
 }

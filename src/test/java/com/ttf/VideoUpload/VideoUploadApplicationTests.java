ebcc24e10cfb801b5275572d6609c375da06c90f
letDouglas <borsettidamirdouglas@gmail.com>
2025-02-13 15:18:06 +0100

    fixing bug-test

--- src/test/java/com/ttf/VideoUpload/VideoUploadApplicationTests.java
+++ src/test/java/com/ttf/VideoUpload/VideoUploadApplicationTests.java
@@ -2,12 +2,14 @@ package com.ttf.VideoUpload;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ComponentScan("com.ttf.VideoUpload")
 class VideoUploadApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

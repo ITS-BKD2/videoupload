3c3015ec6626a33a6dee94d2db56067165f32979
letDouglas <borsettidamirdouglas@gmail.com>
2025-02-13 14:56:03 +0100

    name services to uppercase

--- src/main/java/com/ttf/VideoUpload/controller/VideoUploadRestController.java
+++ src/main/java/com/ttf/VideoUpload/controller/VideoUploadRestController.java
@@ -1,5 +1,6 @@
 package com.ttf.VideoUpload.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +16,24 @@ public class VideoUploadRestController {
 
     private final DiscoveryClient discoveryClient;
     private final RestClient restClient;
+    @Value("${spring.application.name}")
+    private String appName;
 
     public VideoUploadRestController(DiscoveryClient discoveryClient, RestClient.Builder restClientBuilder) {
         this.discoveryClient = discoveryClient;
         restClient = restClientBuilder.build();
     }
 
+
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "Ciao da: " + appName;
+    }
+
     @GetMapping("/getAll")
     public List<String> getAllServices() {
         RestTemplate restTemplate = new RestTemplate();
-        String[] serviceNames = {"VideoUpload", "VideoTranscoding", "servicec"};
+        String[] serviceNames = {"VIDEOUPLOAD", "VIDEOTRANSCODING", "COMMENTS", "USERS", "STREAMING"};
         List<String> allResponses = new ArrayList<>();
 
         for (String serviceName : serviceNames) {
